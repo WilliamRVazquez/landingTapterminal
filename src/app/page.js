5444ab59ec131f575036cd853244214c04bac8e1
WilliamRVazquez <wvazquez0@ucol.mx>
2023-11-12 13:39:08 -0600

    Cambios en el diseño

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 import banner from "@/app/images/slide.jpg";
 import trabajadores from "@/app/images/trabajadores.jpg";
-import mapaPuerto from "@/app/images/mapa_puerto.png"
+import mapaPuerto from "@/app/images/mapa_puerto.png";
 
 import { AiOutlineUser } from "react-icons/ai";
 import { BsFillHouseFill } from "react-icons/bs";
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <main>
       <header>
-      <Navbar />
+        <Navbar />
         <div className="flex relative text-center">
           <Image
             className="w-full h-[500px] md:h-[700px] object-cover"
@@ -30,114 +30,120 @@ export default function Home() {
           </div>
         </div>
       </header>
-      <div className="flex md:flex-row flex-col w-full justify-center gap-9 pt-16">
-        <div className="md:w-1/2">
-          <h2 className="text-3xl font-semibold mb-5">Historia</h2>
-          <p className="mb-4">
-            Líder en el Puerto de Manzanillo en el manejo de carga general,
-            Comercializadora La Junta, y Maniobrista La Junta forman parte de
-            Tap Logística Portuaria, una empresa 100% mexicana que ofrece
-            servicios integrales de carga, descarga, almacenaje y transporte a
-            destino final.
-          </p>
-          <p className="mb-4">
-            Tap Logística Portuaria fue fundada en el año 2006 con inversión
-            100% mexicana y está enfocada en las cadenas logísticas que conectan
-            al Puerto de Manzanillo con el norte y centro de México. Estamos
-            ubicados en Manzanillo, el principal punto de encuentro con el
-            mercado asiático y americano.
-          </p>
-          <p>
-            Somos prestadores de servicios secundarios en el puerto más
-            importante de México: Manzanillo (Colima). En TAP estamos
-            comprometidos a brindar soluciones a nuestros clientes, a través de
-            servicios de calidad y esquemas operativos eficientes en materia de
-            comercio exterior.
-          </p>
-        </div>
-        <div className="w-full md:w-[480px] h-[480px]">
-          <Image className="rounded-md" src={trabajadores} alt="Trabajadores" />
-        </div>
-      </div>
-      <div className="md:px-[220px] mb-10">
-        <div>
-          <h3 className="text-3xl font-semibold mb-5">Valores</h3>
-        </div>
-        <div className="flex md:flex-row flex-col justify-center w-full gap-5">
-          <div className="md:w-1/4 text-center">
-            <div className="flex justify-center">
-              <h3 className="text-8xl text-yellow-300">
-                <AiOutlineUser />
-              </h3>
-            </div>
-            <div>
-              <h4 className="font-semibold my-5">SATISFACCIÓN</h4>
-            </div>
-            <div>
-              <p>
-                Buscamos siempre la satisfacción total de nuestros clientes y
-                nuestro equipo.
-              </p>
-            </div>
+      <div className="container w-full md:flex md:flex-col px-2 mx-auto">
+        <div className="flex flex-col-reverse md:flex-row my-10">
+          <div className="w-full">
+            <h2 className="text-3xl font-semibold mb-5">Historia</h2>
+            <p className="mb-4">
+              Líder en el Puerto de Manzanillo en el manejo de carga general,
+              Comercializadora La Junta, y Maniobrista La Junta forman parte de
+              Tap Logística Portuaria, una empresa 100% mexicana que ofrece
+              servicios integrales de carga, descarga, almacenaje y transporte a
+              destino final.
+            </p>
+            <p className="mb-4">
+              Tap Logística Portuaria fue fundada en el año 2006 con inversión
+              100% mexicana y está enfocada en las cadenas logísticas que
+              conectan al Puerto de Manzanillo con el norte y centro de México.
+              Estamos ubicados en Manzanillo, el principal punto de encuentro
+              con el mercado asiático y americano.
+            </p>
+            <p>
+              Somos prestadores de servicios secundarios en el puerto más
+              importante de México: Manzanillo (Colima). En TAP estamos
+              comprometidos a brindar soluciones a nuestros clientes, a través
+              de servicios de calidad y esquemas operativos eficientes en
+              materia de comercio exterior.
+            </p>
           </div>
-          <div className="md:w-1/4 text-center">
-            <div className="flex justify-center">
-              <h3 className="text-8xl text-yellow-300">
-                <BsFillHouseFill />
-              </h3>
-            </div>
-            <div className="font-semibold my-5">
-            </div>
-              <h4 className="font-semibold my-5">CAPACIDAD</h4>
-            <div>
-              <p>
-                Contamos con más de 35,000 M² de superficie con capacidad para
-                realizar maniobras portuarias y almacenaje de 1,200 TEUS.
-              </p>
-            </div>
-          </div>
-          <div className="md:w-1/4 text-center">
-            <div className="flex justify-center">
-              <h3 className="text-8xl text-yellow-300">
-                <IoExpandSharp />
-              </h3>
-            </div>
-            <div className="font-semibold my-5">
-              <h4>FLEXIBILIDAD</h4>
-            </div>
-            <div>
-              <p>
-                Tenemos la flexibilidad de adaptarnos a nuestros clientes para
-                ser competitivos y eficientes.
-              </p>
-            </div>
-          </div>
-          <div className="md:w-1/4 text-center">
-            <div className="flex justify-center">
-              <h3 className="text-8xl text-yellow-300">
-                <FaHandshake />
-              </h3>
-            </div>
-            <div className="font-semibold my-5">
-              <h4>COMPROMISO</h4>
-            </div>
-            <div>
-              <p>
-                Tenemos compromiso con la calidad de nuestro servicio y la
-                mejora continua de nuestros procesos a través de un modelo de
-                gestión GEMBA basado en el "Toyota Way" que nos permite agregar
-                valor a las operaciones de nuestros clientes.
-              </p>
-            </div>
+          <div className="w-full md:ml-auto">
+            <Image
+              className="rounded-md"
+              src={trabajadores}
+              alt="Trabajadores"
+            />
           </div>
         </div>
-      </div>
-      <div className="md:px-[220px] w-full">
-        <div>
+        <div className="my-10">
+          <h2 className="text-3xl font-semibold mb-5">Valores</h2>
+          <div className="md:flex md:flex-row justify-center gap-5">
+            <div className="w-full text-center border rounded-md shadow-md py-5 px-3">
+              <div className="flex justify-center">
+                <h3 className="text-8xl text-yellow-300">
+                  <AiOutlineUser />
+                </h3>
+              </div>
+              <div>
+                <h4 className="font-semibold my-5">SATISFACCIÓN</h4>
+              </div>
+              <div>
+                <p>
+                  Buscamos siempre la satisfacción total de nuestros clientes y
+                  nuestro equipo.
+                </p>
+              </div>
+            </div>
+            <div className="w-full text-center border rounded-md shadow-md py-5 px-3">
+              <div className="flex justify-center">
+                <h3 className="text-8xl text-yellow-300">
+                  <BsFillHouseFill />
+                </h3>
+              </div>
+              <div className="font-semibold my-5">
+                <h4>CAPACIDAD</h4>
+              </div>
+              <div>
+                <p>
+                  Contamos con más de 35,000 M² de superficie con capacidad para
+                  realizar maniobras portuarias y almacenaje de 1,200 TEUS.
+                </p>
+              </div>
+            </div>
+            <div className="w-full text-center border rounded-md shadow-md py-5 px-3">
+              <div className="flex justify-center">
+                <h3 className="text-8xl text-yellow-300">
+                  <IoExpandSharp />
+                </h3>
+              </div>
+              <div className="font-semibold my-5">
+                <h4>FLEXIBILIDAD</h4>
+              </div>
+              <div>
+                <p>
+                  Tenemos la flexibilidad de adaptarnos a nuestros clientes para
+                  ser competitivos y eficientes.
+                </p>
+              </div>
+            </div>
+            <div className="w-full text-center border rounded-md shadow-md py-5 px-3">
+              <div className="flex justify-center">
+                <h3 className="text-8xl text-yellow-300">
+                  <FaHandshake />
+                </h3>
+              </div>
+              <div className="font-semibold my-5">
+                <h4>COMPROMISO</h4>
+              </div>
+              <div>
+                <p>
+                  Tenemos compromiso con la calidad de nuestro servicio y la
+                  mejora continua de nuestros procesos a través de un modelo de
+                  gestión GEMBA basado en el "Toyota Way" que nos permite
+                  agregar valor a las operaciones de nuestros clientes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
           <h2 className="text-3xl">Mapa del puerto</h2>
-        </div>
-        <div>
-          <Image className="w-full h-auto" src={mapaPuerto} alt="Mapa del puerto" />
+          <div>
+            <Image
+              className="w-full h-auto"
+              src={mapaPuerto}
+              alt="Mapa del puerto"
+            />
+          </div>
         </div>
       </div>
       <Footer />
